badc78be3d3f64f31577dac7bf03a0a63419aeb1
usernamedinesh <borod9200@gmail.com>
2025-08-06 16:05:00 +0530

    adding deleting, adding

--- SELECT.sql
+++ SELECT.sql
@@ -33,3 +33,21 @@ cmd:docker exec -it postgres_prod psql -U postgres -d myapp_admin
 
 how to run this current file
 cmd:docker exec -i postgres_prod psql -U postgres -d myapp_admin < SELECT.sql
+
+-- WHERE
+cmd:SELECT name from users WHERE age <= 30;
+
+
+-- add anther coloum name country
+cmd:ALTER TABLE users
+    ADD COLUMN country VARCHAR(100);
+
+-- assing the user to that country
+UPDATE users
+SET country = 'Indian'
+WHERE name = 'alice';
+-- WHERE id = 1;
+
+-- delete column
+ALTER TABLE table_name
+DROP COLUMN column_name;
