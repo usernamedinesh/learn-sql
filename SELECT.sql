24bbaa9cc5f9c3ecdacf6a1a55f9423e97d32459
usernamedinesh <borod9200@gmail.com>
2025-08-06 15:30:32 +0530

    error handling adding mutiple values

--- SELECT.sql
+++ SELECT.sql
@@ -1,18 +1,17 @@
-/*
-
-create table
+-- create table
 CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
     age INT
 );
 
 
-insert data
+-- insert data
 INSERT INTO  users (name, age) VALUES ('alice', 33);
-INSERT INTO  users (name, age) VALUES ('bob', 30);
+INSERT INTO users (name, age) VALUES ('Alice', 25), ('Bob', 30), ('Charlie', 22);
 
-*/
+
 SELECT
   -- * for all column
   column1,
@@ -20,3 +19,9 @@ SELECT
   column3
 FROM
   table_name;
+
+
+-- list all tables
+\dt
+-- view table scheama
+\d users
