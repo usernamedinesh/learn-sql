80c761f063172f96439893036a62ddac89c63cba
usernamedinesh <borod9200@gmail.com>
2025-08-06 15:40:18 +0530

    adding number data type

---
+++ dataType.sql
@@ -0,0 +1,13 @@
+-- data types
+
+--NUMERIC
+1. INT          => integer
+2. SMALLINT     => Smaller range int
+3. BIGINT       => Larger range of int
+4. DECIMAL(p,s) => fixed-point number with precision & scale
+5. NUMERIC(p,s) => Same as Decimal
+6. FLOAT(p)  => approximate floating point number
+7. REAL => Less precision floating point
+8. DOUBLE => Double precision floating point
+
+-- String (character ) data types
